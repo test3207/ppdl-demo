9e4ea23f0d961ee3a93e1330ab7577da441cd1d9
test3207 <test3207@outlook.com>
2019-11-18 15:54:32 +0800

    update local temp

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const ppdlCore = require('./lib/ppdl-core')
 const args = process.argv
 const provinces = ['jx', 'zj', 'gd', 'xj']
+const fs = require('fs')
+const path = require('path')
+
 let province = 'jx'
 for (let i = 0 ; i < args.length ; i++) {
   if (args[i] === '-p') {
@@ -10,9 +13,11 @@ for (let i = 0 ; i < args.length ; i++) {
 
 if (!provinces.includes(province)) throw new Error('You Have But These Provinces To Choose: jx, zj, gd, xj')
 
+if (!fs.existsSync(path.join(process.cwd(), './temp'))) fs.mkdirSync(path.join(process.cwd(), './temp'))
+
 ppdlCore[province]({
-  serialNo: '123',
+  serialNo: '647067640',
   type: 'fateadm', // 识别器，有验证码的省份需要填
-  payeeName: '张三', // 付款人，新j需要填
+  payeeName: '赵俊', // 付款人，新j需要填
   checkCode: '7777' // 校验码，浙江需要填
 })
