5aaa54f4134bef60df0a8c73044c71bd88c0e61b
test3207 <test3207@outlook.com>
2019-11-18 12:39:14 +0800

    update xj

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const ppdlCore = require('./lib/ppdl-core')
 //   type: 'tesseract'
 // })
 
-ppdlCore.gd({
-  serialNo: '4401071450508**',
-  type: 'fateadm'
-})
+// ppdlCore.gd({
+//   serialNo: '4401071450508**',
+//   type: 'fateadm'
+// })
 
 // ppdlCore.zj({
 //   serialNo: '001201911130161**',
 //   code: 'f764f'
 // })
+
+ppdlCore.xj({
+  serialNo: '647067640',
+  payeeName: '赵俊'
+})
+// http://finpt.xjcz.gov.cn/finpt/billCheck/print.do?billNo=647067640&payeeName=%E8%B5%B5%E4%BF%8A
